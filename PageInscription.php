cde8a2051dbc7c6d3e8193fa34ac52ee8d00db8f
AlexValla <61418446+AlexValla@users.noreply.github.com>
2022-03-13 20:41:00 +0100

    new3

---
+++ PageInscription.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+    <title>Inscription</title>
+</head>
+
+</body> 
+
+
+<form method="post" action="">
+    Email:<input type="text" name="Email"/><br>
+    Mot de passe:<input type="password" name="Password"/><br>
+    <input type="submit" value="Envoyer"/>
+</form>
+</body>
+
+</html>
