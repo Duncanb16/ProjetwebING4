f27c979aa9934e8a6815db65bbdb02f264797032
flojo <florian.jouel@gmail.com>
2022-03-16 23:58:35 +0100

    pas fini il y a un bug daffichage sur la page dacceuil je regle ca dm
    en ouvrant en html ca marche

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WindSell Accueil</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css" />
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
 </head>
 
@@ -19,19 +19,19 @@
     <div class="container-first">
         <h1><span>WindSell </span></h1>
         <div class="container-btns">
-            <a class="btn-first b1" href="boutique.html">Boutique</a>
+            <a class="btn-first b1" href="home_page.html">Boutique</a>
             <a class="btn-first b2">Compte Client</a>
             <a class="btn-first b3">Maps</a>
         </div>
     </div>
 
-    <img src=".\imagesindex\1137px-Google_Maps_Logo_2020.svg.png" class="logo">
+    <img src="images/1137px-Google_Maps_Logo_2020.svg.png" class="logo">
 
 
     <ul class="medias">
-        <li class="bulle"><img src="images\facebook.png" class="logo-medias"></li>
-        <li class="bulle"><img src="images\instagram.png" class="logo-medias"></li>
-        <li class="bulle"><img src="images\youtube.png" class="logo-medias"></li>
+        <li class="bulle"><img src="images/facebook.png" class="logo-medias"></li>
+        <li class="bulle"><img src="images/instagram.png" class="logo-medias"></li>
+        <li class="bulle"><img src="images/youtube.png" class="logo-medias"></li>
     </ul>
 
 
